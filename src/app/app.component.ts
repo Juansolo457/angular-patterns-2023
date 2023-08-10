978312377f8f164580b6f8c67f45fe60e73ae354
Juansolo457 <isitluk@gmail.com>
2023-08-10 08:55:38 -0700

    initial commit - adding summary outlines to add ISG to be on app.component WIP - will move to better folder structure, will clean tsconfig and first conecpts to outline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MockViewModel } from './models/mock-models/mock-view-model';
 
 
 @Component({
@@ -7,18 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  // todo: check this not correct
+  public vm: MockViewModel = {} as MockViewModel
+
   /* #1 View MOdel (VM) implementation */
   // TODO: add mock view model and models Dir with division by domain..
   // public vm: MockViewModel =
   title = 'angular-in-app-styleguide';
 
+  constructor() {}
+
   public ngOnInit() {
-    // TODO: add mock UP of View model
+    this.vm = {
+      name: 'John',
+      guid: '123-456-8DDD',
+      isActive: true,
+      ordinal: 12
+    }
   }
 }
 
-
-export interface MockViewModel {
-  name: string;
-
-}
---
+++ src/app/models/mock-models/mock-view-model.ts
@@ -0,0 +1,7 @@
+export interface MockViewModel {
+  name: string;
+  guid: string;
+  isActive: boolean;
+  ordinal: number;
+
+}
