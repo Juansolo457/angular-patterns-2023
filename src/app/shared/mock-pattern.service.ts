8ee58c7d424c9a9fe0a4271205492eee7f6bfc7c
Juansolo457 <isitluk@gmail.com>
2023-08-15 16:52:46 -0700

    added teardown for service

--- src/app/shared/mock-pattern.service.ts
+++ src/app/shared/mock-pattern.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { MockViewModel } from '../models/mock-models/mock-view-model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MockPatternService {
+export class MockPatternService implements OnDestroy {
+  public unsubscribe$: Subject<void>;
   private mockSubjectPatternExample$: BehaviorSubject<MockViewModel> = new BehaviorSubject<MockViewModel>(undefined);
   private _mockPrimitiveNumber: boolean;
+
+  public constructor() {
+    this.unsubscribe$ = new Subject<void>();
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next(undefined);
+    this.unsubscribe$.complete();
+  }
 
   public get getMockPatternExample(): BehaviorSubject<MockViewModel> {
     return this.mockSubjectPatternExample$;
