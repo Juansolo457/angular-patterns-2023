aeebc2089c592daa318b84b1961bf7f388f888ed
Juansolo457 <isitluk@gmail.com>
2023-08-15 16:25:44 -0700

    fixed es lint and prettier too work on html and typescript files, verified rules are working app wide on saving of project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
+import { NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const bootstrap: any = (): Promise<NgModuleRef<AppModule>> => platformBrowserDynamic().bootstrapModule(AppModule);
+bootstrap().catch((err: any): void => console.log(err)); // eslint-disable-line no-console
